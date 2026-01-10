0cb851368bc12345476c86d2885a3a35af233d1b
CJ <cjfritz9@gmail.com>
2026-01-10 18:11:15 -0500

    remove unused import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 import AuthWrapper from "@/components/auth/AuthWrapper";
 import { LoginForm } from "@/components/auth/LoginForm";
-import { Loader, LoadingOverlay } from "@mantine/core";
+import { Loader } from "@mantine/core";
 import React, { Suspense } from "react";
 
 const LoginPage: React.FC = async () => {
